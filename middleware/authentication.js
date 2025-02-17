0e7ac2631e859141282865e585d7b1c1c843fb06
08abhinav <abhinavnegi101@gmail.com>
2025-02-17 18:30:05 +0530

    user authentication and authorization middleware created

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,17 +1,31 @@
-import { validateToken } from "../services/authorization.js";
+import { validateToken, validateUserToken } from "../services/authorization.js";
 
 export function checkForAuthentication(cookieName){
     return(req, res, next)=>{
         const tokenCookieValue = req.cookies[cookieName];
         if(!tokenCookieValue) return next();
-
         try {
             const authorPayload = validateToken(tokenCookieValue)
-            req.user = authorPayload
+            req.author = authorPayload
             
         } catch (error) {
             res.json({message: "authentication failure"})
         }
         return next();
+    }
+}
+
+export function checkForUserAuthentication(userCookie){
+    return (req, res, next)=>{
+        const userTokenCookieValue = req.cookies[userCookie]
+        if(!userTokenCookieValue) return next();
+
+        try {
+            const userPayload = validateUserToken(userTokenCookieValue)
+            req.user = userPayload
+        } catch (error) {
+            res.json({message: "authentication failure"})
+        }
+        return next()
     }
 }
--- services/authorization.js
+++ services/authorization.js
@@ -1,7 +1,10 @@
 import jwt from "jsonwebtoken"
 
 const secret = "$ecret$uperM@N_69"
+const userSecret = "$ecretU$ER__"
 
+
+//Author's token creation
 export function createToken(author){
     
     const payload = {
@@ -16,4 +19,22 @@ export function createToken(author){
 export function validateToken(token){
     const payload = jwt.verify(token, secret)
     return payload
+}
+
+
+
+//User's token creation
+export function createUserToken(user){
+   const userPayload = {
+    _id: user._id,
+    email: user.email
+   } 
+
+   const userToken = jwt.sign(userPayload, userSecret)
+   return userToken
+}
+
+export function validateUserToken(userToken){
+    const userPayload = jwt.verify(userToken, userSecret)
+    return userPayload
 }
